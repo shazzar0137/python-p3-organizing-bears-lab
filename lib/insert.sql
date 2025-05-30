3580a0817fd5467c144328441c295126dc99907d
Daniel Kipngetich <danielkipngetich10@gmail.com>
2025-05-30 10:14:59 +0300

    updates create.sql, insert.sql and sql_queries.py

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 10, "M", "black", "warm", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 30, "M", "white", "warm", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 10, "F", "red", "warm", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 10, "M", "black white", "warm", FALSE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 10, "F", "brown", "warm", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 10, "M", "green", "warm", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 10, "F", "gray", "warm", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES (NULL, 10, "M", "black", "warm", TRUE);
